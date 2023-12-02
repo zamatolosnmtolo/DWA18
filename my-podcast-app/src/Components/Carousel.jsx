9dde8b1bbd622eae68d064c7ad350eeea66e4f8e
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-12-02 15:10:04 +0200

    Update my-podcast-app

--- my-podcast-app/src/Components/Carousel.jsx
+++ my-podcast-app/src/Components/Carousel.jsx
@@ -6,7 +6,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Carousel.css';
 
-// NextArrow and PrevArrow components here
 const NextArrow = ({ onClick }) => (
   <button className="arrow next" onClick={onClick}></button>
 );
@@ -15,25 +14,24 @@ const PrevArrow = ({ onClick }) => (
   <button className="arrow prev" onClick={onClick}></button>
 );
 
-
 export default function PodcastCarousel() {
-  // State
   const [recommendedPodcasts, setRecommendedPodcasts] = useState([]);
-  const [setExpandedDescriptions] = useState({});
+  const [expandedDescriptions, setExpandedDescriptions] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  // Fetch podcast information from the API on component mount
   useEffect(() => {
     axios
       .get('https://podcast-api.netlify.app/shows')
       .then((response) => {
         setRecommendedPodcasts(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching podcasts:', error);
+        setLoading(false);
       });
   }, []);
 
-  // Handle "Read More" click
   const handleReadMoreClick = (podcastId) => {
     setExpandedDescriptions((prevState) => ({
       ...prevState,
@@ -41,7 +39,6 @@ export default function PodcastCarousel() {
     }));
   };
 
-  // Slider settings
   const sliderSettings = {
     dots: false,
     infinite: true,
@@ -54,15 +51,27 @@ export default function PodcastCarousel() {
   return (
     <div className="carousel-container">
       <h1 className='recomended-podcasts'>Recommended Podcasts</h1>
-      <Slider {...sliderSettings}>
-        {recommendedPodcasts.map((podcast) => (
-          <div key={podcast.id} className="carousel-item">
-            <Link to={`/podcast/${podcast.id}`}>
-              <img src={podcast.image} alt={podcast.title} />
-            </Link>
-          </div>
-        ))}
-      </Slider>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Slider {...sliderSettings}>
+          {recommendedPodcasts.map((podcast) => (
+            <div key={podcast.id} className="carousel-item">
+              <Link to={`/podcast/${podcast.id}`}>
+                <img src={podcast.image} alt={podcast.title} />
+              </Link>
+              {expandedDescriptions[podcast.id] ? (
+                <p>{podcast.description}</p>
+              ) : (
+                <p>{podcast.shortDescription}</p>
+              )}
+              <button onClick={() => handleReadMoreClick(podcast.id)}>
+                Read More
+              </button>
+            </div>
+          ))}
+        </Slider>
+      )}
     </div>
   );
 }
--- my-podcast-app/src/Components/Hero.jsx
+++ my-podcast-app/src/Components/Hero.jsx
@@ -4,8 +4,8 @@ import './Hero.css'
 export default function Header() {
     return (
         <div className='hero'>
-            <h1 className='hero--title'>Cast of Pods</h1>
-            <h2 className='hero--subtitle'>All the Podcasts You Need, In One Place.</h2>
+            <h1 className='hero--title'>Soundify</h1>
+            <h2 className='hero--subtitle'>Your Podcast Heaven, Welcome </h2>
         </div>
     )
 }
