6053804662ce12b1ee6ab183a61f24839c2dc057
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-12-04 08:26:29 +0200

    Updating comments

--- my-podcast-app/src/Components/Hero.jsx
+++ my-podcast-app/src/Components/Hero.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
 
-export default function Header() {
-    return (
-        <div className='hero'>
-            <h1 className='hero--title'>Soundify</h1>
-            <h2 className='hero--subtitle'>Your Podcast Heaven, Welcome </h2>
-        </div>
-    )
+/**
+ * Header Component for the hero section.
+ * Displays the title and subtitle for the "Soundify" website.
+ */
+const Header = () => {
+  return (
+    <div className='hero'>
+      {/* Main title of the hero section */}
+      <h1 className='hero--title'>Soundify</h1>
+      {/* Subtitle welcoming users to the podcast heaven */}
+      <h2 className='hero--subtitle'>Your Podcast Heaven, Welcome</h2>
+    </div>
+  );
 }
+
+export default Header;
